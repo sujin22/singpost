c52375fd4cd4b55187a7517a25ea84600a9c6edd
sujin22 <aoghk3333@gmail.com>
2024-03-15 16:38:32 +0900

    update: 240315 project ver

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/singpost-route/",
+  base: "/singpost-deploy/",
   server: {
     hmr:
       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
