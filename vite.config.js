1be405e583bde811e54ebb22279b7bf3e4c99a26
sujin22 <aoghk3333@gmail.com>
2024-03-15 14:35:20 +0900

    update: 240315_project ver

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/singpost-route/",
+  server: {
+    hmr:
+      process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
+        ? 443
+        : undefined,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        schedule: "./schedule.html",
+        detail: "./detail.html",
+        current: "./current_schedule.html"
+      },
+    },
+  },
+});
