4514b205af817bd9c50c88f23f50a1772833de76
sujin22 <aoghksj@naver.com>
2024-04-08 09:20:06 +0900

    update: 240408 modal 추가 버전

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/singpost-deploy/",
+  base: "/singpost-route/",
   server: {
     hmr:
       process.env.CODESANDBOX_SSE || process.env.GITPOD_WORKSPACE_ID
